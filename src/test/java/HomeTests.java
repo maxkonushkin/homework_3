40439decdb1b6560e7661ee9f0ec8a7faaa67187
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-01 22:31:18 +0400

    add any_steps_6

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -40,7 +40,6 @@ public class HomeTests {
         $("#state").click();
         $("#state").pressEnter();
         sleep(3000);
-
         $("#subjectsContainer").pressEnter();
         $("#hobbies-checkbox-1").click();
         $("[class=col-md-9 col-sm-12]").setValue("inf");
