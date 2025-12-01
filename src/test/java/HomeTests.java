23e7cd087a76ebde6a73db7db23dcb0d892d50cf
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-01 22:22:54 +0400

    add any_steps_5

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class homework_3 {
+public class HomeTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
