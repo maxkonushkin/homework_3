90bf3616dbd5f20c826443a9e9d98d74fb5b842e
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-01 22:23:28 +0400

    add any_steps_5

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -44,7 +44,6 @@ public class HomeTests {
         $("#hobbies-checkbox-1").click();
         $("[class=col-md-9 col-sm-12]").setValue("inf");
 
-
         sleep(3000);
         $("#output #name").shouldHave(text("Alex"));
         $("#output #email").shouldHave(text("alex@egorov.com"));
