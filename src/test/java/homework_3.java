86d29c887a7ca18fec248bc0c9c5a2f588999924
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-01 22:19:49 +0400

    add any_steps_3

--- src/test/java/homework_3.java
+++ src/test/java/homework_3.java
@@ -40,10 +40,8 @@ public class homework_3 {
         $("#state").click();
         $("#state").pressEnter();
         sleep(3000);
-
         $("#subjectsContainer").pressEnter();
         $("#hobbies-checkbox-1").click();
-
 
         $("[class=col-md-9 col-sm-12]").setValue("inf");
 
