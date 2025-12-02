edd8a5b752e00624e93fb3084b957537819d2df4
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-02 13:28:53 +0400

    finish

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -18,6 +18,8 @@ public class HomeTests {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Maks");
         $("#lastName").setValue("Konushkin");
         $("#userEmail").setValue("myemail@example.com");
@@ -29,11 +31,25 @@ public class HomeTests {
         $(".react-datepicker__month").$(byText("11")).click();
         $("#genterWrapper").$(byText("Male")).click();
         $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#state").click();
         $(byText("Haryana")).click();
         $("#city").click();
         $(byText("Panipat")).click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#uploadPicture").uploadFromClasspath("Cat.jpg");
         $("#submit").click();
-        sleep(9000);
+        //Проверки:
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Maks Konushkin"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("myemail@example.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9173173687"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("11 July,1996"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Maths, Computer Science"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Reading"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("Cat.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("proizvodstvennai 13"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Haryana Panipat"));
     }
 }
