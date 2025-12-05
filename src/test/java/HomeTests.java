2e0a0e2f84e8054fa5455c216531ddbfa03f9a56
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-05 17:07:02 +0400

    add_Page

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -1,56 +1,94 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class HomeTests {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 1000; // default 4000
-    }
+public class HomeTests extends TestBase{
+
+    RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
     void fillFormTest() {
-        open("/automation-practice-form");
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-        $("#firstName").setValue("Maks");
-        $("#lastName").setValue("Konushkin");
-        $("#userEmail").setValue("myemail@example.com");
-        $("#userNumber").setValue("9173173687");
-        $("#currentAddress").setValue("proizvodstvennai 13");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$(byText("July")).click();
-        $(".react-datepicker__year-select").$(byText("1996")).click();
-        $(".react-datepicker__month").$(byText("11")).click();
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#hobbiesWrapper").$(byText("Reading")).click();
+
+        registrationPage
+                .openPage()
+                .setFirstName("Maks")
+                .setLastName("Konushkin")
+                .setEmail("myemail@example.com")
+                .setUserNumber("9173173687")
+                .setCurrentAddress("proizvodstvennai 13")
+                .setGender("Male")
+                .setDateOfBirth("11", "July", "1996")
+                .setUploadPicture()
+                .setSubjectsInput()
+                .setHobbiesWrapper()
+               // .setSubmit()
+                ;
+        //Проверки:
+        sleep(3000);
+
+
         $("#state").click();
         $(byText("Haryana")).click();
         $("#city").click();
         $(byText("Panipat")).click();
-        $("#subjectsInput").setValue("Maths").pressEnter();
-        $("#subjectsInput").setValue("Computer Science").pressEnter();
-        $("#uploadPicture").uploadFromClasspath("Cat.jpg");
+
         $("#submit").click();
 
         //Проверки:
-        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Maks Konushkin"));
-        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("myemail@example.com"));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9173173687"));
-        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("11 July,1996"));
-        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Maths, Computer Science"));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Reading"));
-        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("Cat.jpg"));
-        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("proizvodstvennai 13"));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Haryana Panipat"));
+        registrationPage
+                .checkResult("Student Name", "Maks Konushkin")
+                .checkResult("Student Email", "myemail@example.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "9173173687")
+                .checkResult("Date of Birth", "11 July,1996")
+                .checkResult("Subjects", "Computer Science")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "Cat.jpg")
+                .checkResult("Address", "proizvodstvennai 13")
+                .checkResult("State and City", "Haryana Panipat")
+
+        ;
+
+    }
+
+    @Test
+    void minFormTest() {
+
+        registrationPage
+                .openPage()
+                .setFirstName("Maks")
+                .setLastName("Konushkin")
+                .setUserNumber("9173173687")
+                .setGender("Male")
+                .setSubmit()
+        ;
+        //Проверки:
+        registrationPage
+                .checkResult("Student Name", "Maks Konushkin")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "9173173687")
+        ;
+
+    }
+    @Test
+    void negativeFormTest() {
+
+        registrationPage
+                .openPage()
+                .setFirstName("Maks")
+                // Проверяем обязательность поля setLastName
+                // .setLastName("Konushkin")
+                .setUserNumber("9173173687")
+                .setGender("Male")
+                .setSubmit()
+        ;
+        //Проверки:
+        registrationPage
+                .checkResult("Student Name", "Maks")
+        ;
+
     }
 }
