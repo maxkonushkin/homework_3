de23c0d698857323fe17a75bf296dbe79aa56aa7
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-12-09 19:07:18 +0400

    add Faker

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -1,11 +1,28 @@
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
 import static com.codeborne.selenide.Selenide.sleep;
 
 public class HomeTests extends TestBase{
-
+    Faker faker = new Faker();
     RegistrationPage registrationPage = new RegistrationPage();
+
+    String firstname = faker.name().firstName();
+    String lastName = new String("Konushkin");
+    String email = new String("myemail@example.com");
+    String userNumber = new String("9173173687");
+    String currentAddress = new String("proizvodstvennai 13");
+    String gender = new String("Male");
+    String dateOfBirth1 = new String("11");
+    String dateOfBirth2 = new String("July");
+    String dateOfBirth3 = new String("1996");
+    String uploadPicture = new String("Cat.jpg");
+    String subjectsInput = new String("Computer Science");
+    String hobbiesWrapper = new String("Sports");
+    String state = new String("Haryana");
+    String city = new String("Panipat");
+
 
     @Test
     void fillFormTest() {
@@ -13,21 +30,20 @@ public class HomeTests extends TestBase{
         registrationPage
                 .openPage()
                 .hidebanner()
-                .setFirstName("Maks")
-                .setLastName("Konushkin")
-                .setEmail("myemail@example.com")
-                .setUserNumber("9173173687")
-                .setCurrentAddress("proizvodstvennai 13")
-                .setGender("Male")
-                .setDateOfBirth("11", "July", "1996")
-                .setUploadPicture("Cat.jpg")
-                .setSubjectsInput("Computer Science")
-                .setHobbiesWrapper("Sports")
-                .setState("Haryana")
-                .setCity("Panipat")
+                .setFirstName(firstname)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setUserNumber(userNumber)
+                .setCurrentAddress(currentAddress)
+                .setGender(gender)
+                .setDateOfBirth(dateOfBirth1, dateOfBirth2, dateOfBirth3)
+                .setUploadPicture(uploadPicture)
+                .setSubjectsInput(subjectsInput)
+                .setHobbiesWrapper(hobbiesWrapper)
+                .setState(state)
+                .setCity(city)
                 .setSubmit()
                 ;
-        sleep(4000);
         //Проверки:
         registrationPage
                 .checkResult("Student Name", "Maks Konushkin")
@@ -49,10 +65,10 @@ public class HomeTests extends TestBase{
 
         registrationPage
                 .openPage()
-                .setFirstName("Maks")
-                .setLastName("Konushkin")
-                .setUserNumber("9173173687")
-                .setGender("Male")
+                .setFirstName(firstname)
+                .setLastName(lastName)
+                .setUserNumber(userNumber)
+                .setGender(gender)
                 .setSubmit()
         ;
         //Проверки:
@@ -68,11 +84,11 @@ public class HomeTests extends TestBase{
 
         registrationPage
                 .openPage()
-                .setFirstName("Maks")
+                .setFirstName(firstname)
                 // Проверяем обязательность поля setLastName
-                // .setLastName("Konushkin")
-                .setUserNumber("9173173687")
-                .setGender("Male")
+                // .setLastName(lastName)
+                .setUserNumber(userNumber)
+                .setGender(gender)
                 .setSubmit()
         ;
         //Проверки:
